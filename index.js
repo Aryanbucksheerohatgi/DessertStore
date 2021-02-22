ef0a4c3afda6723038ca01d0f05de8aee79431a2
Aryan <aryanbrohatgi@gmail.com>
2021-02-22 17:24:40 +0530

    nit an array

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-let maintainence=()=>{
-    alert("still working on it")
+// let maintainence=()=>{
+//     alert("still working on it")
 
+// }
+let cart = [];
+let cartadd = (temp) => {
+  cart.push(temp)
+  console.log(cart)
 }
