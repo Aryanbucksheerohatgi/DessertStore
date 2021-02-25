60bbcea15f8563729b898ebb67c46b156970017d
Aryan <aryanbrohatgi@gmail.com>
2021-02-25 17:32:22 +0530

    finale

--- index.js
+++ index.js
@@ -11,3 +11,29 @@ let cartadd = (temp) => {
     cart[temp] += 1;
   }
 }
+
+let temp = document.getElementsByClassName("mycard")
+ console.log(temp)
+ let maintainence1=()=>
+ {
+  for(const i of temp){
+    i.style.display = "flex"
+
+  }
+  
+ }
+
+ let maintainence2=()=>
+ {
+  for(const i of temp){
+  if(i.classList.contains("cake")){
+    i.style.display = "flex"
+  
+
+  }
+  else{
+    i.style.display = "none"
+  }
+  
+ }
+}
